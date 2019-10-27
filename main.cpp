8f0f88db3b15ba1071e8ef647ef87397335645ef
araaf2ndaccount <arofgg3@gmail.com>
2019-10-27 19:56:16 +0700

    third commit

---
+++ main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string.h>
+#include <cstdlib>
+#include <ctime>
+
+/*
+    Nama : Mamet Adil Araaf
+    Nim  : A11.2017.10656
+*/
+
+using namespace std;
+
+int main()
+{
+    string kata;
+    int in_rand,temp;
+
+    cout << "--- Tebak Kata ---" << endl ;
+
+    cout << "Masukkan Kata : ";
+    cin >> kata;
+
+    string awal= kata;
+
+    for (int i=0; i<awal.size(); i++){
+        in_rand = rand() % kata.size();
+        temp = kata[i];
+        kata[i] = kata[in_rand];
+        kata[in_rand] = temp;
+    }
+    cout << "Kata Setelah Diacak : " << kata <<endl;
+
+
+    cout << "--- Tebak Kata ---"<< endl;
+    string jwb;
+
+    cout << "Masukkan tebakan : " ;
+    cin >> jwb;
+
+    if(awal.compare(jwb)==0){
+        cout << "Selamat anda benar !!";
+    }
+    else{
+        cout << "Anda Salah !!";
+
+    }
+    return 0;
+}
